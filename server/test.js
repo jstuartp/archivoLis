52b90e4a3ed193704f3a3670689554936dca4b00
jstuartp <jstuartp@gmail.com>
2025-09-16 14:24:49 -0600

    Delete server directory

--- server/test.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const { readLisHeader, pgaMax } = require('./lisParser');
-
-const file = path.join(__dirname, 'events/sampleEvent/sample1.Lis');
-const header = readLisHeader(file);
-console.log('Header keys:', Object.keys(header));
-console.log('PGA max:', pgaMax(header));
